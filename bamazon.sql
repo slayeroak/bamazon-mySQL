5831b6884c0e6e2fc6751fe9c0eef3c8569cd369
slayeroak <roakley1@villanova.edu>
2020-03-13 08:24:13 -0400

    populated the database with ten products

--- bamazon.sql
+++ bamazon.sql
@@ -25,3 +25,20 @@ CREATE TABLE products {
     stock_quantity INT(10) NOT NULL,
     PRIMARY KEY (item_id)
 }
+
+-- Populate this database with around 10 different products.
+
+SELECT * FROM products;
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (001, "half day 4 pack", "inshore", 250, 365),
+        (002, "half day 4 pack", "offshore", 300, 365),
+        (003, "full day 4 pack", "inshore", 500, 365),
+        (004, "full day 4 pack", "offshore", 600, 365),
+        (005, "half day 6 pack", "inshore", 350, 365),
+        (006, "half day 6 pack", "offshore", 450, 365),
+        (007, "full day 6 pack", "inshore", 700, 365),
+        (008, "half day 4 pack", "inshore", 900, 365),
+        (009, "fishing pole", "rental", 20, 365),
+        (010, "jumbo shrimp(12)", "bait", 8, 365)
+
